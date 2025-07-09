4dfa5364f79a7339548e20a0dc17e713565aab54
Mohamed <Mohamed@DESKTOP-CJQ1NVC>
2025-07-09 15:36:50 +0300

    cors3

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ import cors from "cors";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json({ limit: "2mb" }));
 app.use(express.urlencoded({ extended: true, limit: "2mb" }));
 app.use("/uploads", express.static(path.join(__dirname, "./uploads")));
