06584b5ebf3504967e16043dae5af05848c626ed
Mohamed <Mohamed@DESKTOP-CJQ1NVC>
2025-07-09 15:50:54 +0300

    cors4

--- src/server.ts
+++ src/server.ts
@@ -12,6 +12,9 @@ app.use(cors({ origin: "*" }));
 app.use(express.json({ limit: "2mb" }));
 app.use(express.urlencoded({ extended: true, limit: "2mb" }));
 app.use("/uploads", express.static(path.join(__dirname, "./uploads")));
+app.use("/api/test", (req, res, next) => {
+  res.json({ message: "API is working!" });
+});
 app.use("/api", ApiRoute);
 app.use((req, res, next) => {
   throw new NotFound("Route not found");
