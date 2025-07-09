9294f64a8276e80566159b4d5ba3ba7596df4c81
Mohamed <Mohamed@DESKTOP-CJQ1NVC>
2025-07-09 15:33:04 +0300

    cors2

--- dist/drizzle.config.js
+++ dist/drizzle.config.js
@@ -3,10 +3,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 require("dotenv/config");
 const drizzle_kit_1 = require("drizzle-kit");
 exports.default = (0, drizzle_kit_1.defineConfig)({
-  out: "./drizzle",
-  schema: "./src/models/schema.js",
-  dialect: "mysql",
-  dbCredentials: {
-    url: process.env.DATABASE_URL,
-  },
+    out: "./drizzle",
+    schema: "./src/models/schema.ts",
+    dialect: "mysql",
+    dbCredentials: {
+        url: process.env.DATABASE_URL,
+    },
 });
