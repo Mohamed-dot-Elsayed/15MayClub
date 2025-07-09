4e19f6884f83add45a258217802b0f413632bc19
Mohamed <Mohamed@DESKTOP-CJQ1NVC>
2025-07-09 15:31:24 +0300

    cors

--- src/server.ts
+++ src/server.ts
@@ -4,10 +4,11 @@ import ApiRoute from "./routes";
 import { errorHandler } from "./middlewares/errorHandler";
 import { NotFound } from "./Errors";
 import dotenv from "dotenv";
+import cors from "cors";
 dotenv.config();
 
 const app = express();
-
+app.use(cors());
 app.use(express.json({ limit: "2mb" }));
 app.use(express.urlencoded({ extended: true, limit: "2mb" }));
 app.use("/uploads", express.static(path.join(__dirname, "./uploads")));
